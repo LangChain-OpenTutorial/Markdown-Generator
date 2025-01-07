fd2ed6d2b8ed284446491c827ef1925fd33fd3d3
sooyoung-wind <sooyoung.wind@gmail.com>
2025-01-08 04:00:44 +0900

    🔧 create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="markdown-generator",
+    version="0.1.0",
+    description="Converter from Jupyter Notebook(.ipynb) to Markdown(.md)",
+    author="sooyoung-wind, teddylee777",
+    author_email="sooyoung.wind@gmail.com, teddylee777@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "nbconvert>=7.16.5",
+    ],
+    python_requires=">=3.11",
+)
